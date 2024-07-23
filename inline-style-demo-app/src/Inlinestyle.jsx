5f3e9d29c5fec9780237b3bda5a98a0ceda80c0d
Vagheladhaval <139227532+Vagheladhaval@users.noreply.github.com>
2024-07-23 17:50:58 +0530

    23-07-2024

---
+++ inline-style-demo-app/src/Inlinestyle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function app()
+{
+  return(
+    <>
+     <div>
+        <h1>Hwy I am used inline stylesheet</h1>
+        <p>Hey i am just used paragraph</p>
+     </div>
+    </>
+  )
+}
+export default app;
---
+++ inline-style-demo-app/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import app from "./Inlinestyle";
+
+ReactDOM.createRoot(document.getElementById("demo")).render(
+    <>
+    <app />
+    </>
+)
